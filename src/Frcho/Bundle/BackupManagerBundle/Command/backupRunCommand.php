cceec96471c4c79c7fe0ed3f171a14289d18008d
frcho <luisfer1g@gmail.com>
2016-02-22 16:58:50 -0500

    changes in command backups .. we can send parameter to this command
    database
    destination
    destinationFileName
    compression

--- src/Frcho/Bundle/BackupManagerBundle/Command/backupRunCommand.php
+++ src/Frcho/Bundle/BackupManagerBundle/Command/backupRunCommand.php
@@ -4,6 +4,7 @@ namespace Frcho\Bundle\BackupManagerBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\StringInput;
 use BackupManager\Filesystems\Destination;
@@ -14,6 +15,10 @@ class backupRunCommand extends ContainerAwareCommand {
         $this
                 ->setName('backup:manager:backup')
                 ->setDescription('Runs backup database config')
+                ->addArgument('database', InputArgument::OPTIONAL, 'Database configuration name', null)
+                ->addArgument('destination', InputArgument::OPTIONAL, 'Destination configuration name', null)
+                ->addArgument('destinationFileName', InputArgument::OPTIONAL, 'File destination path', null)
+                ->addArgument('compression', InputArgument::OPTIONAL, 'Compression type: gzip, null', null)
         ;
     }
 
@@ -21,12 +26,44 @@ class backupRunCommand extends ContainerAwareCommand {
 
 
         $container = $this->getContainer();
-        $container->get('backup_manager')->makeBackup()->run('development', array(
-            new Destination('local', 'backup/test.sql'),
+
+        $date = new \DateTime();
+        $result = $date->format('Y-m-d H:i:s');
+        $fileName = $container->getParameter('database_name') . '-' . $result;
+
+        $database = $input->getArgument('database');
+
+        if (!empty($database)) {
+            $database = $input->getArgument('database');
+        } else {
+            $database = 'development';
+        }
+
+        $destination = $input->getArgument('destination');
+        if (!empty($destination)) {
+            $destination = $input->getArgument('destination');
+        } else {
+            $destination = 'local';
+        }
+
+        $destinationFileName = $input->getArgument('destinationFileName');
+        if (!empty($destinationFileName)) {
+            $destinationFileName = $input->getArgument('destinationFileName');
+        } else {
+            $destinationFileName = $fileName;
+        }
+
+        $compression = $input->getArgument('compression');
+        if (!empty($compression)) {
+            $compression = $input->getArgument('compression');
+        } else {
+            $compression = 'null';
+        }
+
+        $container->get('backup_manager')->makeBackup()->run($database, array(
+            new Destination($destination, 'backups/' . $destinationFileName . '.sql'),
 //            new Destination('s3', 'backups/test.sql')
-            ), 'null');
-//        \Symfony\Component\VarDumper\VarDumper::dump($t6est);
-//        die();
+                ), $compression);
     }
 
 }
