1c9e07a32c747132cf8e240455cb781e116e4afd
frcho <lgranados@kijho.com>
2016-08-09 12:03:51 -0500

    upload to s3 if credentials exist

--- Command/backupRunCommand.php
+++ Command/backupRunCommand.php
@@ -2,12 +2,16 @@
 
 namespace Frcho\Bundle\BackupManagerBundle\Command;
 
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Input\StringInput;
+use Symfony\Component\Yaml\Parser;
+use Symfony\Component\Config\FileLocator;
 use BackupManager\Filesystems\Destination;
+use Symfony\Component\Console\Input\StringInput;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 
 class backupRunCommand extends ContainerAwareCommand {
 
@@ -63,9 +67,62 @@ class backupRunCommand extends ContainerAwareCommand {
         $container->get('backup_manager')->makeBackup()->run($database, array(
             new Destination($destination, 'backups/' . $destinationFileName . '.sql'),
                 ), $compression);
-//        $container->get('backup_manager')->makeBackup()->run($database, array(
-//            new Destination('s3', $destinationFileName . '.sql')
-//                ), $compression);
+
+        $this->uploadS3($database, $destinationFileName, $compression);
+        
+        $this->removeLocalFiles();
+    }
+
+    protected function uploadS3($database, $destinationFileName, $compression) {
+        $container = $this->getContainer();
+
+        $yaml = new Parser();
+        $content = $yaml->parse(file_get_contents($container->get('kernel')->getRootDir() . '/config/config.yml'));
+
+        if ($content["frcho_backup_manager"]) {
+            $params = $content["frcho_backup_manager"]["storage"]["s3"];
+            $i = 0;
+            $stdS3 = true;
+
+            foreach ($params as $contentFile) {
+                if (!empty($contentFile)) {
+
+                    $paramKey = str_replace("%", "", $contentFile);
+
+                    if (in_array($i, [1, 2, 3, 4]) && empty($container->getParameter($paramKey))) {
+                        $stdS3 = FALSE;
+                        break;
+                    }
+
+                    $i++;
+                }
+            }
+            if ($stdS3) {
+                $container->get('backup_manager')->makeBackup()->run($database, array(
+                    new Destination('s3', 'backups/' . $destinationFileName . '.sql')
+                        ), $compression);
+            }
+        }
+    }
+
+    protected function removeLocalFiles() {
+        $container = $this->getContainer();
+
+        $path = $container->getParameter('kernel.root_dir') . '/data/backups/';
+
+        $handle = opendir($path);
+        if ($handle !== false) {
+
+            while (false !== ($file = readdir($handle))) {
+                $filelastmodified = filemtime($path . $file);
+                //24 hours in a day * 3600 seconds per hour
+                if ((time() - $filelastmodified) > 20 * 24 * 3600) {
+                    unlink($path . $file);
+                }
+            }
+
+            closedir($handle);
+        }
     }
 
 }
